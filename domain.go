365b8dc002410dba83b6a4db889ac8d88508f22e
contre95 <lucascontre95@gmail.com>
2023-10-06 13:21:21 +0200

    Meme, Saviour

--- domain.go
+++ domain.go
@@ -1,12 +1,17 @@
 package main
 
+type Size float32
+
 type Meme struct {
-	Size float32
+	// Image []byte // Not sure if we are gonna need this.
+	Size Size
 	Path string
+	Name string
 }
 
 type Saviour interface {
 	Save(m Meme) error
 	Name() string
+	MaxSize() Size
 	GetRandomMeme() (*Meme, error)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module meme-saviour
+
+go 1.21.1
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
